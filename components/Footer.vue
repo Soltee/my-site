f1e369355d89ca043a957ec95eb92e28e1708b25
Prabin Gurung <37467008+Soltee@users.noreply.github.com>
2022-02-13 14:43:15 +0545

    added updated resume and fixed contact section css styles

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,14 +1,14 @@
 <template>
       <footer id="footer" class="relative  ">
-        <div class="py-6  max-w-6xl mx-auto px-6">
+        <div class="py-6 max-w-6xl mx-auto px-6">
           <div class="mb-3">
-            <div class="text-1100 flex flex-col md:flex-row">
+            <div class="text-1100 flex flex-col md:flex-row md:items-center">
               <span class="text-xl font-semibold text-3xl md:text-6xl">Reach out to me on</span>
               <a 
-                class="md:ml-6 border-1100 rounded-full  p-2 border-2" href="mailto:meprabingrg@gmail.com">
+                class="md:ml-6 border-1100 w-12 h-12 rounded-full p-2 border-2" href="mailto:meprabingrg@gmail.com">
                 <svg 
                   xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" 
-                  class="h-8 w-8 md:h-12 md:w-12 hover:opacity-50 "><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
+                  class="h-auto w-auto  hover:opacity-50 "><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline></svg>
               </a>
             </div>
           </div>
